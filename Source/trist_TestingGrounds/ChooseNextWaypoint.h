fb6fc3857d5d42d03a20e882324ea61601f4573c
Bartlomiej Wolk <bartlomiejwolk@gmail.com>
2019-05-09 14:47:05 +0200

    Access Blackboard Index key from cpp class

--- Source/trist_TestingGrounds/ChooseNextWaypoint.h
+++ Source/trist_TestingGrounds/ChooseNextWaypoint.h
@@ -17,4 +17,7 @@ class TRIST_TESTINGGROUNDS_API UChooseNextWaypoint : public UBTTaskNode
 public:
 	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
 	
+protected:
+	UPROPERTY(EditAnywhere, Category = "Blackboard")
+	struct FBlackboardKeySelector IndexKey;
 };
